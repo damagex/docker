5ad5eeccf394150301068964e17f1634173b4ac5
damagex <davem1991@gmail.com>
2021-12-19 18:08:43 +0100

    Update entrypoint.sh

--- app/csi/live/entrypoint.sh
+++ app/csi/live/entrypoint.sh
@@ -8,8 +8,6 @@ echo "Current timezone: $(cat /etc/timezone)"
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-./run.sh
-
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
