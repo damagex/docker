7c4e89889d717d6664232a566d9bb0b7772360d3
damagex <davem1991@gmail.com>
2021-12-19 17:13:21 +0100

    Create entrypoint.sh

---
+++ app/csi/live/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd /home/container
+
+# Information output
+echo "Running on Debian $(cat /proc/version)"
+echo "Current timezone: $(cat /etc/timezone)"
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+./run.sh
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
