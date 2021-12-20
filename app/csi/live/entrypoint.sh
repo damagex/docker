d1c699d3869ef62ef141d98a4340b83dbcad2d6b
damagex <davem1991@gmail.com>
2021-12-20 14:48:04 +0100

    Update entrypoint.sh

--- app/csi/live/entrypoint.sh
+++ app/csi/live/entrypoint.sh
@@ -12,7 +12,9 @@ meteor --version
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
+cd /home/container
 MONGO_URL={{MONGO_URL}}
 meteor run --port {{SERVER_IP}}:{{SERVER_PORT}}
+
 # Run the Server
 # eval ${MODIFIED_STARTUP}
