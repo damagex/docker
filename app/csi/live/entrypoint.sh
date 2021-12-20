7df2d8a533380af1398d7d1f75328dc3b39fd6f0
damagex <davem1991@gmail.com>
2021-12-20 11:36:03 +0100

    Update entrypoint.sh

--- app/csi/live/entrypoint.sh
+++ app/csi/live/entrypoint.sh
@@ -8,6 +8,9 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 node -v
 meteor --version
 
+chown -Rh container /home/container
+chown -Rh container /home/container/src/.meteor/local
+
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
